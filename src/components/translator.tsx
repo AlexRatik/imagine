24ad2b5c45b8254ddc1a7b85f81274c6c94550e7
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-03 21:32:40 +0300

    feat: create translator

---
+++ src/components/translator.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { useAppDispatch, useAppSelector } from "../hooks/storeHooks";
+import { Select } from "./select/select";
+import { setLanguage, TranslatorType } from "../features/translatorSlice";
+
+const StyledTranslator = styled.div`
+    position: relative;
+`;
+
+export function Translator() {
+    const { language } = useAppSelector((store) => store.translator);
+    const dispatch = useAppDispatch();
+
+    return (
+        <StyledTranslator>
+            <Select
+                values={["ENG", "RUS"]}
+                selectedValue={language}
+                name="translator"
+                width={50}
+                height={27}
+                onSelect={(value: string) =>
+                    dispatch(setLanguage(value as TranslatorType))
+                }
+            />
+        </StyledTranslator>
+    );
+}
