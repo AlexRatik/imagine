9370629af5a0f13534d58b4cc660bad0db37085e
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-04 01:44:03 +0300

    fix: fix translator

--- src/components/translator.tsx
+++ src/components/translator.tsx
@@ -4,25 +4,25 @@ import { Select } from "./select/select";
 import { setLanguage, TranslatorType } from "../features/translatorSlice";
 
 const StyledTranslator = styled.div`
-    position: relative;
+  position: relative;
 `;
 
 export function Translator() {
-    const { language } = useAppSelector((store) => store.translator);
-    const dispatch = useAppDispatch();
+  const { language } = useAppSelector((store) => store.translator);
+  const dispatch = useAppDispatch();
 
-    return (
-        <StyledTranslator>
-            <Select
-                values={["ENG", "RUS"]}
-                selectedValue={language}
-                name="translator"
-                width={50}
-                height={27}
-                onSelect={(value: string) =>
-                    dispatch(setLanguage(value as TranslatorType))
-                }
-            />
-        </StyledTranslator>
-    );
+  return (
+    <StyledTranslator>
+      <Select
+        values={["ENG", "RUS"]}
+        selectedValue={language}
+        name="translator"
+        width={58}
+        height={27}
+        onSelect={(value: string) =>
+          dispatch(setLanguage(value as TranslatorType))
+        }
+      />
+    </StyledTranslator>
+  );
 }
