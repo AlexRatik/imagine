ea49b1f50fe8ece63aaf1dbaffd1aeac4d55fe99
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-04 13:51:46 +0300

    fix: fix header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import { NavList } from "../navList/navList";
+import { ReactComponent as HomeIcon } from "../../assets/home.svg";
+import { ChatIcon } from "../chatIcon";
+import { ReactComponent as SettingsIcon } from "../../assets/settings.svg";
+import { ReactComponent as UserIcon } from "../../assets/userpic.svg";
+import { Translator } from "../translator";
 
 const StyledHeader = styled.header`
   padding: 0 255px;
@@ -11,11 +16,19 @@ const StyledHeader = styled.header`
   box-shadow: 0 10px 30px 4px rgba(85, 105, 135, 0.03);
 `;
 
+const icons = [
+  <HomeIcon />,
+  <ChatIcon />,
+  <SettingsIcon />,
+  <UserIcon />,
+  <Translator />,
+];
+
 export function Header() {
   return (
     <StyledHeader>
       <Logo />
-      <NavList />
+      <NavList values={icons} width={312} />
     </StyledHeader>
   );
 }
