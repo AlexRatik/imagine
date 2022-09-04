45e3a241b42b77b9b1433078473005623f275eae
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-04 13:54:37 +0300

    feat: add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,50 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { App } from "./App";
-import "./index.css";
+import { createGlobalStyle } from "styled-components";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  ul, ol {
+    list-style-type: none;
+  }
+
+  a{
+    color: inherit;
+    text-decoration: none;
+    transition: all 0.2s ease-in-out;
+    &:visited {
+      color: inherit;
+    }
+    &:hover {
+      opacity: 0.6;
+    }
+  }
+`;
+
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <GlobalStyles />
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
