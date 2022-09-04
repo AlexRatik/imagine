e984f501f71321953d63ec3e8c6072e140243777
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-04 13:55:15 +0300

    feat: add footer into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/header/header";
 import Main from "./components/main";
+import { Footer } from "./components/footer/footer";
 
 export function App() {
   return (
-    <div>
+    <>
       <Header />
       <Main />
-    </div>
+      <Footer />
+    </>
   );
 }
